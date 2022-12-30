17572f10ec76001a3cf75a81cd1110a7d5196656
issy <issy@issy.dev>
2022-12-30 14:11:51 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="jsondataclasses",
+    url="https://github.com/issy/python-jsondataclasses",
+    license="MIT",
+    packages=["jsondataclasses"],
+    version="0.0.1a",
+    python_requires=">=3.9.0"
+)
