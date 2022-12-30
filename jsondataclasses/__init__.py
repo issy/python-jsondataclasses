2e7633465501ca5faca52c5014c286004cc7511b
issy <issy@issy.dev>
2022-12-30 16:08:59 +0000

    Lint

--- jsondataclasses/__init__.py
+++ jsondataclasses/__init__.py
@@ -6,10 +6,7 @@ __all__ = ("jsonfield", "jsondataclass")
 
 
 def jsonfield(
-    key: str,
-    parser: Optional[Callable[[Any], Any]] = None,
-    *,
-    default_value: Any = None,
+    key: str, parser: Optional[Callable[[Any], Any]] = None, *, default_value: Any = None
 ) -> tuple[str, Optional[Callable[[Any], Any]], Any]:
     return key, parser, default_value
 
@@ -18,55 +15,31 @@ _T = TypeVar("_T")
 
 
 def parse_field(
-    data: dict,
-    json_key: str,
-    field_type: type[_T],
-    field_parser: Optional[Callable[[Any], _T]],
-    default_value: Any,
+    data: dict, json_key: str, field_type: type[_T], field_parser: Optional[Callable[[Any], _T]], default_value: Any
 ) -> _T:
     type_of_type = type(field_type)
 
-    if (
-        json_key not in data
-        and type_of_type is not typing._UnionGenericAlias
-        and default_value is not None
-    ):
+    if json_key not in data and type_of_type is not typing._UnionGenericAlias and default_value is not None:
         return (field_parser or field_type)(default_value)
     elif type_of_type is type:
         if field_parser:
             return field_parser(data[json_key])
         else:
             return data[field_type(json_key)]
-    elif type_of_type is typing._UnionGenericAlias and isinstance(
-        None, field_type.__args__[1]
-    ):  # Parse Optional[Any]
-        return (
-            parse_field(
-                data, json_key, field_type.__args__[0], field_parser, default_value
-            )
-            if json_key in data
-            else None
-        )
+    elif type_of_type is typing._UnionGenericAlias and isinstance(None, field_type.__args__[1]):  # Parse Optional[Any]
+        return parse_field(data, json_key, field_type.__args__[0], field_parser, default_value) if json_key in data else None
     elif type_of_type is typing._LiteralGenericAlias:  # Parse Literal[...]
         # TODO: Check that parsed value is in literal args
-        parsed_value = parse_field(
-            data, json_key, type(field_type.__args__[0]), field_parser, default_value
-        )
+        parsed_value = parse_field(data, json_key, type(field_type.__args__[0]), field_parser, default_value)
         if parsed_value not in field_type.__args__:
-            raise ValueError(
-                f"{getattr(data, json_key, default_value)} not of literal {field_type.__args__!r}"
-            )
+            raise ValueError(f"{getattr(data, json_key, default_value)} not of literal {field_type.__args__!r}")
         return parsed_value
     elif type_of_type is types.GenericAlias:  # Parse list[Any]
         return [(field_parser or field_type.__args__[0])(i) for i in data[json_key]]
 
 
 def jsondataclass(cls: type) -> type:
-    fields = {
-        k: v
-        for k, v in cls.__dict__.items()
-        if not (k.startswith("__") and k.endswith("__"))
-    }
+    fields = {k: v for k, v in cls.__dict__.items() if not (k.startswith("__") and k.endswith("__"))}
     field_types = {k: t for k, t in cls.__annotations__.items() if k in fields}
 
     def __init__(self, data: dict):
@@ -75,18 +48,11 @@ def jsondataclass(cls: type) -> type:
             setattr(
                 self,
                 key,
-                parse_field(
-                    data, field_meta[0], field_type, field_meta[1], field_meta[2]
-                ),
+                parse_field(data, field_meta[0], field_type, field_meta[1], field_meta[2]),
             )
 
     def __repr__(self: cls) -> str:
-        return (
-            cls.__name__
-            + "("
-            + ", ".join(f"{k}={getattr(self, k)!r}" for k in fields)
-            + ")"
-        )
+        return cls.__name__ + "(" + ", ".join(f"{k}={getattr(self, k)!r}" for k in fields) + ")"
 
     cls.__init__ = __init__
     cls.__repr__ = __repr__
